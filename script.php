729cf44c588869eb1e290ff421bc0b0e416c52e9
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-14 10:36:46 -0400

    Echoes fizzbuzz

---
+++ script.php
@@ -0,0 +1,16 @@
+<?php
+
+for($x = 1; $x <= 100; $x++){
+  if($x % 15 == 0){
+    echo("FizzBuzz");
+  }else if($x % 3 == 0){
+    echo("Fizz");
+  }else if($x % 5 == 0){
+    echo("Buzz");
+  }else{
+    echo($x);
+  }
+  echo("\n");
+}
+
+?>
